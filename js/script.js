cd371876f8e0ed5ac6055ca3861ac9f1f91cad13
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-12-05 20:24:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,52 @@
-let result = function (res = 0) {
+/** @format */
+
+function result(
+	res = 0,
+	statisticIncr = 0,
+	statisticDecr = 0,
+	statisticGet = 0
+) {
 	const func = {
 		increase(x) {
-			return res += x;
+			statisticIncr += 1;
+			return (res += x);
 		},
 		decrease(x) {
-			return res -= x;
+			statisticDecr += 1;
+			return (res -= x);
 		},
 		get() {
+			statisticGet += 1;
 			return res;
-		}
+		},
+		getStatistic() {
+			return (statistic = {
+				incr: statisticIncr,
+				decr: statisticDecr,
+				get: statisticGet,
+			});
+		},
 
-	}
+		getStatisticZero() {
+			return (statistic = {
+				incr: (statisticIncr = 0),
+				decr: (statisticDecr = 0),
+				get: (statisticGet = 0),
+			});
+		},
+		zero() {
+			return (res = 0);
+		},
+	};
 	return func;
 }
-let inst = result();
+const inst = result();
 
 inst.increase(100);
+inst.increase(100);
+inst.decrease(30);
 inst.decrease(30);
 console.log(inst.get());
+console.log(inst.getStatistic());
+console.log(inst.getStatisticZero());
+console.log(inst.getStatistic());
